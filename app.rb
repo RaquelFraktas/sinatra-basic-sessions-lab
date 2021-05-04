ed1594ba498850d400495bc2a2c6d9ee73cd4fef
github username <github email address>
2021-05-03 22:51:24 -0400

    commit tree 22db850661f812fa40db6b7859371a6ef1bca224

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+      end 
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+ 
+    session[:item] = params[:item]
+    #i added an items key to sessions
+    @session = session
+    #set a global variable for the current session hash with the item that was added
+    
+    erb :checkout
+  end
+
 end
